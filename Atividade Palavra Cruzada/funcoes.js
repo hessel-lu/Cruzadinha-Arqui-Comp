8a30e4ba1592d8336d43dddaf8e6c35d91f24944
hessel-lu <lucas.hessel@sptech.school>
2022-05-12 21:02:44 -0300

    crizadinha atualizada 12.05 às 21:00

---
+++ Atividade Palavra Cruzada/funcoes.js
@@ -0,0 +1,70 @@
+    // Funções dos botões/dicas:
+    function buttonUm() {
+        alert("Dica:")
+    }
+
+    function buttonDois() {
+        alert("Dica:")
+    }
+
+    function buttonTres() {
+        alert("Dica:")
+    }
+
+    function buttonQuatro() {
+        alert("Dica:")
+    }
+
+    function buttonCinco() {
+        alert("Dica:")
+    }
+
+    function buttonSeis() {
+        alert("Dica:")
+    }
+
+    function buttonSete() {
+        alert("Dica:")
+    }
+
+    function buttonOito() {
+        alert("Dica:")
+    }
+
+    function buttonNove() {
+        alert("Dica:")
+    }
+
+    function buttonDez() {
+        alert("Dica:")
+    }
+
+    function buttonOnze() {
+        alert("Dica")
+    }
+
+    function buttonDoze() {
+        alert("Dica:")
+    }
+
+    function buttonTreze() {
+        alert("Dica:")
+    }
+
+    function buttonQuatorze() {
+        alert("Dica:")
+    }
+
+    function buttonQuinze() {
+        alert("Dica:")
+    }
+
+    function buttonDezesseis() {
+        alert("Dica:")
+    }
+    // fim das funções dos botões/dicas.
+
+    
+    // Validações das imputs:
+
+
